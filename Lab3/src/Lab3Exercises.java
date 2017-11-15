959e1d25c97d14ccae0c01916e4d23e86742041d
4015d3 <4015d3@granada.ece.uprm.edu>
2017-11-15 14:10:37 -0400

    Testing Testing 1...2...3..

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -3,9 +3,11 @@ import javax.swing.JFrame;
 public class Lab3Exercises {
 	public static void main(String[] args) {
 		JFrame myFrame = new JFrame("Hello, world!");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
-		myFrame.setSize(400, 100);
+	    myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		myFrame.setLocation(850, 380);
+		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
+		myFrame.setTitle("Guillermo Betancourt");
 	}
+	
 }
